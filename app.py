93dd89576cef44b8df676160d7da25d9ce1fb90d
George <george4n@gmail.com>
2020-04-28 17:39:17 +0300

    Initial commit

---
+++ app.py
@@ -0,0 +1,18 @@
+import pymongo
+from pymongo import MongoClient
+import datetime
+
+client = MongoClient()
+db = client['test-database']
+
+collection = db.test_collection
+
+post = {"author": "George",
+        "text": "My first blog post!",
+        "tags": ["mongodb", "python", "pymongo"],
+        "date": datetime.datetime.utcnow()}
+
+posts = db.posts
+articles = db.articles
+post_id = posts.insert_one(post).inserted_id
+print(db.list_collection_names())
